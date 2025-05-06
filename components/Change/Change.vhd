83034cf7a74938f3afe7a8f103fbfed8a8b2673c
IceGent17 <79372827+IceGent17@users.noreply.github.com>
2025-05-06 14:18:20 +0200

    Update Change.vhd

--- components/Change/Change.vhd
+++ components/Change/Change.vhd
@@ -3,10 +3,10 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity Change is
     Port ( clk : in std_logic;
-           Btn_1 : in STD_LOGIC;  --  long UP / Down button
-           Btn_2 : in STD_LOGIC;  -- long C button
+           Input_pulse_1 : in STD_LOGIC;  --  long UP / Down button
+           Input_pulse_2 : in STD_LOGIC;  -- long C button
            rst : in STD_LOGIC;    -- C button
-           mode: in std_logic_vector (1 downto 0);
+           current_mode: in std_logic_vector (1 downto 0);
            change_0 : out STD_LOGIC;
            change_1 : out STD_LOGIC;
            change_2 : out std_logic;
@@ -26,20 +26,20 @@ begin
         if rst='1' then
             sig_ch1 <= '0';
             sig_ch2 <= '0';
-       elsif ((Btn_1 = '1')AND (sig_ch2 = '0')) then
+       elsif ((Input_pulse_1 = '1')AND (sig_ch2 = '0')) then
             sig_ch1 <= '1';
-        elsif ((Btn_2 = '1')AND (sig_ch1 = '0')) then
+        elsif ((Input_pulse_2 = '1')AND (sig_ch1 = '0')) then
             sig_ch2 <= '1';
         end if;
         
-        s_mode <= mode;
+        s_mode <= current_mode;
        
     end if;
 end process;
 
-change_0 <= '1' when ((mode="00") AND (sig_ch1 = '1')) else  '0'; -- clock change
-change_1 <= '1' when ((mode="00") AND (sig_ch2= '1')) else  '0';  -- alarm change
-change_2 <= '1' when ((mode="10") AND (sig_ch1 = '1')) else  '0'; -- timer change
+change_0 <= '1' when ((s_mode="00") AND (sig_ch1 = '1')) else  '0'; -- clock change
+change_1 <= '1' when ((s_mode="00") AND (sig_ch2= '1')) else  '0';  -- alarm change
+change_2 <= '1' when ((s_mode="10") AND (sig_ch1 = '1')) else  '0'; -- timer change
 
 change_active <= sig_ch2 OR sig_ch1;
 
