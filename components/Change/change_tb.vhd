65511a4dc53e1f5b7a195a357f6b206fd392b8b8
IceGent17 <79372827+IceGent17@users.noreply.github.com>
2025-05-06 14:22:51 +0200

    Update change_tb.vhd

--- components/Change/change_tb.vhd
+++ components/Change/change_tb.vhd
@@ -8,10 +8,10 @@ architecture tb of tb_Change is
 
     component Change
         port (clk           : in std_logic;
-              Btn_1       : in std_logic;
-              Btn_2         : in std_logic;
+              Input_pulse_1       : in std_logic;
+              Input_pulse_2         : in std_logic;
               rst           : in std_logic;
-              mode: in std_logic_vector (1 downto 0);
+              current_mode: in std_logic_vector (1 downto 0);
               change_0      : out std_logic;
               change_1      : out std_logic;
               change_2      : out std_logic;
@@ -19,16 +19,16 @@ architecture tb of tb_Change is
     end component;
 
     signal clk           : std_logic;
-    signal Btn_1         : std_logic;
-    signal Btn_2         : std_logic;
+    signal Input_pulse_1         : std_logic;
+    signal Input_pulse_2         : std_logic;
     signal rst           : std_logic;
-    signal mode          : std_logic_vector(1 downto 0);
+    signal current_mode          : std_logic_vector(1 downto 0);
     signal change_0      : std_logic;
     signal change_1      : std_logic;
     signal change_2      : std_logic;
     signal change_active : std_logic;
 
-    constant TbPeriod : time := 10 ns; 
+    constant TbPeriod : time := 10 ns;
     signal TbClock : std_logic := '0';
     signal TbSimEnded : std_logic := '0';
 
@@ -36,58 +36,50 @@ begin
 
     dut : Change
     port map (clk           => clk,
-              Btn_1       => Btn_1,
-              Btn_2         => Btn_2,
+              Input_pulse_1       => Input_pulse_1,
+              Input_pulse_2         => Input_pulse_2,
               rst           => rst,
-              mode          => mode,
+              current_mode          => current_mode,
               change_0      => change_0,
               change_1      => change_1,
               change_2      => change_2,
               change_active => change_active);
 
-
+    -- Clock generation
     TbClock <= not TbClock after TbPeriod/2 when TbSimEnded /= '1' else '0';
-
-
     clk <= TbClock;
 
     stimuli : process
     begin
-        
-        Btn_1 <= '0';
-        Btn_2 <= '0';
-        mode  <= "00";
-
+        Input_pulse_1 <= '0';
+        Input_pulse_2 <= '0';
+        current_mode  <= "00";
         rst <= '1';
         wait for 100 ns;
         rst <= '0';
         wait for 100 ns;
         
-        Btn_1 <= '1';
+        Input_pulse_1 <= '1';
         wait for 10 * TbPeriod;
-        Btn_1 <= '0';
-        Btn_2 <= '1';
+        Input_pulse_1 <= '0';
+        Input_pulse_2 <= '1';
         wait for 10 * TbPeriod;
-        Btn_2 <= '0';
+        Input_pulse_2 <= '0';
         rst <= '1';
         wait for 10 * TbPeriod;
         rst <= '0';
-        Btn_2 <= '1';
+        Input_pulse_2 <= '1';
         wait for 10 * TbPeriod;
-        Btn_2 <= '0';
+        Input_pulse_2 <= '0';
         rst <= '1';
-        Btn_1 <= '1';
+        Input_pulse_1 <= '1';
         wait for 10 * TbPeriod;
         rst<= '0';
         wait for 10 * TbPeriod;
-        mode <= "01";
+        current_mode <= "01";
         wait for 10 * TbPeriod;
-        mode <= "10";
-        
-        wait for 10 * TbPeriod;  
-
+        current_mode <= "10";
         wait for 100 * TbPeriod;
-
         TbSimEnded <= '1';
         wait;
     end process;
