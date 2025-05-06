d59decd7b195ac8444263f4c6dc0fda4e3a4388b
IceGent17 <79372827+IceGent17@users.noreply.github.com>
2025-05-06 12:40:50 +0200

    Update UD_counter_tb.vhd

--- components/UD counter/UD_counter_tb.vhd
+++ components/UD counter/UD_counter_tb.vhd
@@ -1,8 +1,3 @@
--- Testbench automatically generated online
--- at https://vhdl.lapinoo.net
--- Generation date : Thu, 10 Apr 2025 15:36:30 GMT
--- Request id : cfwk-fed377c2-67f7e57e09fcb
-
 library ieee;
 use ieee.std_logic_1164.all;
 
@@ -20,7 +15,7 @@ architecture tb of tb_UD_counter is
               clk : in std_logic;
               rst : in std_logic;
               en  : in std_logic;
-              v   : out std_logic_vector (NBITS-1 downto 0));
+              count   : out std_logic_vector (NBITS-1 downto 0));
     end component;
     constant NBITS: integer := 2;
     signal add : std_logic;
@@ -28,9 +23,9 @@ architecture tb of tb_UD_counter is
     signal clk : std_logic;
     signal rst : std_logic;
     signal en  : std_logic;
-    signal v   : std_logic_vector (NBITS-1 downto 0);
+    signal count   : std_logic_vector (NBITS-1 downto 0);
 
-    constant TbPeriod : time := 10 ns; -- ***EDIT*** Put right period here
+    constant TbPeriod : time := 10 ns; 
     signal TbClock : std_logic := '0';
     signal TbSimEnded : std_logic := '0';
 
@@ -42,30 +37,23 @@ begin
               sub => sub,
               clk => clk,
               rst => rst,
-              v   => v,
+              count   => count,
               en => en);
 
-    -- Clock generation
     TbClock <= not TbClock after TbPeriod/2 when TbSimEnded /= '1' else '0';
-
-    -- ***EDIT*** Check that clk is really your main clock signal
     clk <= TbClock;
 
     stimuli : process 
     begin
-        -- ***EDIT*** Adapt initialization as needed
         add <= '0';
         sub <= '0';
-
-        -- Reset generation
-        -- ***EDIT*** Check that rst is really your reset signal
+        en <= '1';
         rst <= '1';
         wait for 10 ns;
         rst <= '0';
         wait for 10 ns;
 
-        -- ***EDIT*** Add stimuli here
-        en <= '1';
+        
         wait for 10 * TbPeriod;
         add <= '1';
         sub <= '0';
@@ -83,16 +71,17 @@ begin
         wait for 10 * TbPeriod;
         add<='0';
         sub<='0';
+        wait for 10 * TbPeriod;
+        en<='1';
+        add<='1';
+        sub<='1';
+        wait for 10 * TbPeriod;
 
-        -- Stop the clock and hence terminate the simulation
         TbSimEnded <= '1';
         wait;
     end process;
 
 end tb;
-
--- Configuration block below is required by some simulators. Usually no need to edit.
-
 configuration cfg_tb_UD_counter of tb_UD_counter is
     for tb
     end for;
