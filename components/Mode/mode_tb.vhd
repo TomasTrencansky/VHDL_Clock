2f8d07a7d0a1cd02bdbb7863f1be96e2ac64faf1
IceGent17 <79372827+IceGent17@users.noreply.github.com>
2025-05-06 13:28:52 +0200

    Update mode_tb.vhd

--- components/Mode/mode_tb.vhd
+++ components/Mode/mode_tb.vhd
@@ -15,26 +15,21 @@ architecture tb of tb_Mode is
               en    : in std_logic;
               btn_l : in std_logic;
               btn_r : in std_logic;
-              CLK100MHZ : in STD_LOGIC;
-              
+              CLK100MHZ : in STD_LOGIC;   
             --  p_test : out std_logic;
             --  p_test2 : out std_logic;
-              
-              m  : out std_logic_vector (1 downto 0)
-               
+              current_mode  : out std_logic_vector (1 downto 0)
            );
               
     end component;
   
     signal en: std_logic;
-    
     signal p_test :  std_logic;
     signal p_test2 :  std_logic;
-    
     signal btn_l : std_logic;
     signal btn_r : std_logic;
     signal clk   : std_logic;
-    signal m  : std_logic_vector (1 downto 0);
+    signal current_mode  : std_logic_vector (1 downto 0);
     constant Ntime: integer := 5;
     constant TbPeriod : time := 10 ns; -- ***EDIT*** Put right period here
     signal TbClock : std_logic := '0';
@@ -50,21 +45,45 @@ begin
               btn_r => btn_r,
               CLK100MHZ =>clk,
               en => en,
-             
               --p_test => p_test,
              -- p_test2=> p_test2,
-              m  => m
+              current_mode  => current_mode
              );
 
     -- Clock generation
     TbClock <= not TbClock  after TbPeriod/2 when TbSimEnded /= '1' else '0';
-    -- ***EDIT*** Check that clk is really your main clock signal
     clk <= TbClock;
    
     stimuli : process
     begin
         en<='1';
-        -- ***EDIT*** Adapt initialization as needed
+        btn_l <= '1';
+        btn_r <= '0';
+        wait for 5*TbPeriod;
+        btn_l <= '0';
+        btn_r <= '0';
+        wait for 1*TbPeriod;
+        btn_l <= '1';
+        btn_r <= '0';
+        wait for 11*TbPeriod;
+        btn_l <= '0';
+        btn_r <= '0';
+        wait for 1*TbPeriod;
+        btn_l <= '0';
+        btn_r <= '1';
+        wait for 5*TbPeriod;
+        btn_l <= '0';
+        btn_r <= '0';
+        wait for 1*TbPeriod;
+        btn_l <= '0';
+        btn_r <= '1';
+        wait for 15*TbPeriod;
+        en <= '1';
+        btn_l <= '0';
+        btn_r <= '0';
+        wait for 1*TbPeriod;
+        
+        en<='0';
         btn_l <= '1';
         btn_r <= '0';
         wait for 5*TbPeriod;
@@ -91,17 +110,15 @@ begin
         btn_r <= '0';
         wait for 1*TbPeriod;
 
-        -- ***EDIT*** Add stimuli here
+
         wait for 100 * TbPeriod;
 
-        -- Stop the clock and hence terminate the simulation
         TbSimEnded <= '1';
         wait;
     end process;
 
 end tb;
 
--- Configuration block below is required by some simulators. Usually no need to edit.
 
 configuration cfg_tb_Mode of tb_Mode is
     for tb
